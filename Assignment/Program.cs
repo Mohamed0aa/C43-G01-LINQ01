5125f05f718e1514908d392c6226107f2888e239
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-16 04:58:34 +0200

    q1

---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using static Assignment.ListGenerator;
+namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1=>for fluent
+            //2=> for query
+
+            #region Find all products that are out of stock.
+            //1
+            var result = ProductList.Where(p => p.UnitsInStock == 0);
+
+            //2
+            result=from p in ProductList
+                   where p.UnitsInStock == 0
+                   select p;
+
+            foreach (var item in result) 
+                Console.WriteLine(item);
+            #endregion
+        }
+    }
+}
