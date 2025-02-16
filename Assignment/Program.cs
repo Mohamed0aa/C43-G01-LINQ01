5f91bb60b7ff2b6f3db11cea2883b25a531e0542
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-16 05:02:16 +0200

    q2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -9,18 +9,34 @@ namespace Assignment
             //1=>for fluent
             //2=> for query
 
-            #region Find all products that are out of stock.
-            //1
-            var result = ProductList.Where(p => p.UnitsInStock == 0);
+            #region q1 : Find all products that are out of stock.
+            ////1
+            //var result = ProductList.Where(p => p.UnitsInStock == 0);
 
-            //2
-            result=from p in ProductList
-                   where p.UnitsInStock == 0
-                   select p;
+            ////2
+            //result=from p in ProductList
+            //       where p.UnitsInStock == 0
+            //       select p;
 
-            foreach (var item in result) 
-                Console.WriteLine(item);
+            //foreach (var item in result) 
+            //    Console.WriteLine(item);
             #endregion
+
+
+            #region q2 :Find all products that are in stock and cost more than 3.00 per unit.
+
+            ////1
+            //var result = ProductList.Where(p => p.UnitsInStock == 0 &&p.UnitPrice>(decimal)3.00);
+
+            ////2
+            //result = from p in ProductList
+            //         where p.UnitsInStock == 0 && p.UnitPrice > (decimal)3.00
+            //         select p;
+
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+
+                #endregion
         }
     }
 }
