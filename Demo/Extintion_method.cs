f5adb4ddeedee5335fec88c6e8d17557114802b5
Kimo-Store <Kimo Store@DESKTOP-RTPEEP9>
2025-02-16 04:45:28 +0200

    ex

---
+++ Demo/Extintion_method.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo
+{
+    internal static class Extension_method
+    {
+
+        public static int Reverse(this int x)
+        {
+            int r = 0, rr = 0;
+            while (x > 0)
+            {
+                r = x % 10;
+
+                rr = rr * 10 + r;
+                x /= 10;
+            }
+            return rr;
+        }
+    }
+}
